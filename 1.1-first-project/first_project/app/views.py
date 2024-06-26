ddd8488863b39641a7e4901b27e5391d680948c1
SRS <rs.sku@yandex.ru>
2024-06-26 16:30:53 +0300

    Task done

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
+import datetime
+import os
 
 
 def home_view(request):
@@ -8,10 +10,10 @@ def home_view(request):
     # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
     }
-    
+
     # context и параметры render менять не нужно
     # подбробнее о них мы поговорим на следующих лекциях
     context = {
@@ -23,7 +25,7 @@ def home_view(request):
 def time_view(request):
     # обратите внимание – здесь HTML шаблона нет, 
     # возвращается просто текст
-    current_time = None
+    current_time = datetime.datetime.now()
     msg = f'Текущее время: {current_time}'
     return HttpResponse(msg)
 
@@ -32,4 +34,9 @@ def workdir_view(request):
     # по аналогии с `time_view`, напишите код,
     # который возвращает список файлов в рабочей 
     # директории
-    raise NotImplemented
+    files_list = os.listdir(".")
+    files_list = "; ".join(files_list)
+
+    return HttpResponse(files_list)
+    # raise NotImplemented
+
