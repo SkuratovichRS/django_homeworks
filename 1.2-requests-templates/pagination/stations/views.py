ad82e8877e77179c6ac7bf879d1456ea34636e89
SRS <rs.sku@yandex.ru>
2024-07-04 13:18:57 +0300

    task pagination done

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,5 +1,9 @@
+import csv
+
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.core.paginator import Paginator
+from pagination.settings import BUS_STATION_CSV
 
 
 def index(request):
@@ -9,9 +13,19 @@ def index(request):
 def bus_stations(request):
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
-
+    data = []
+    with open(BUS_STATION_CSV, encoding="utf-8") as f:
+        r = csv.DictReader(f)
+        for row in r:
+            data.append(row)
+    paginator = Paginator(data, 5)
+    page_number = int(request.GET.get("page", 1))
+    page = paginator.get_page(page_number)
+    stations = [{"Name": station.get("Name"),
+                 "Street": station.get("Street"),
+                 "District": station.get("District")} for station in page]
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': stations,
+        'page': page,
     }
     return render(request, 'stations/index.html', context)
