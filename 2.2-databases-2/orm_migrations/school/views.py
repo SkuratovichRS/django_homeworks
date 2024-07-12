6e48faf08297a73a7ff6401bd8b35682e45a0902
SRS <rs.sku@yandex.ru>
2024-07-12 18:01:39 +0300

    Task orm_migrations done

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -6,10 +6,10 @@ from .models import Student
 
 def students_list(request):
     template = 'school/students_list.html'
-    context = {}
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = 'group'
+    students_objects = Student.objects.order_by(ordering).prefetch_related('teachers')
+    context = {"students": students_objects}
 
     return render(request, template, context)
+
+
