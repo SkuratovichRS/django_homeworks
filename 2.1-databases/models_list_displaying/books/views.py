0f5905cee23a05a6dc3ceba8890c1d49e8babe40
SRS <rs.sku@yandex.ru>
2024-07-12 12:24:54 +0300

    Task models_list_displaying done

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render
+from books.models import Book
+from django.core.paginator import Paginator
 
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    books_objects = Book.objects.all()
+    context = {"books": books_objects}
+    return render(request, template, context)
+
+
+def pub_date_view(request, pub_date):
+    template = 'books/books_pub_date.html'
+    book_object = Book.objects.get(pub_date=pub_date)
+    books_objects = Book.objects.all().order_by('pub_date')
+    paginator = Paginator(books_objects, 1)
+    current_page_number = [index + 1 for index, data in enumerate(books_objects)
+                           if data.pub_date == book_object.pub_date][0]
+    page = paginator.get_page(current_page_number)
+    previous_page_number = page.previous_page_number() if page.has_previous() else None
+    next_page_number = page.next_page_number() if page.has_next() else None
+
+    previous_book = books_objects[previous_page_number - 1] if previous_page_number else None
+    next_book = books_objects[next_page_number - 1] if next_page_number else None
+
+    context = {
+        "book": book_object,
+        "page": page,
+        "previous_book": previous_book,
+        "next_book": next_book,
+    }
+
     return render(request, template, context)
--- 2.1-databases/models_list_displaying/main/urls.py
+++ 2.1-databases/models_list_displaying/main/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from books.views import books_view
+from books.views import books_view, pub_date_view
 
 urlpatterns = [
-    path('', books_view, name='books'),
+    path('books/', books_view, name='books'),
     path('admin/', admin.site.urls),
+    path('books/<pub_date>/', pub_date_view, name='pub_date'),
 ]
